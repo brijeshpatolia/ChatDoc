e2852f0e59619e1644b7da8613f4240ca47c708b
brijeshpatolia <bpatolialtc@gmail.com>
2025-01-09 11:36:58 +0530

    small changes in  ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ export default async function Home() {
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <div className="flex flex-col items-center text-center">
           <div className="flex items-center">
-            <h1 className="mr-4 text-6xl font-semibold">Lick any PDF</h1>
+            <h1 className="mr-4 text-6xl font-semibold">chat with any document</h1>
             <UserButton appearance={userButtonAppearance} />
           </div>
 
